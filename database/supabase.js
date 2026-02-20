a863aee3ea1fca46658b3a5438dbf2ee8d9904f7
Sam Lanier <selanier0523@gmail.com>
2026-02-19 21:59:49 -0500

    had to rotate keys cuz they got leaked :(

--- database/supabase.js
+++ database/supabase.js
@@ -5,7 +5,7 @@ const { createClient } = require("@supabase/supabase-js");
 // Use environment variables
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseServiceKey =
-  process.env.SUPABASE_SERVICE_ROLE_KEY;
+  process.env.supabase_service_role_key;
 
 // IMPORTANT:
 // Use the SERVICE ROLE key in the backend.
